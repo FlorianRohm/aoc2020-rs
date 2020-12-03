10d1199c9b09076baab82aa8d9dcfc64673d2f84
Florian Rohm <rohm.florian@googlemail.com>
2020-12-03 07:18:43 +0100

    day 3

---
+++ day3/src/main.rs
@@ -0,0 +1,111 @@
+use std::convert::TryFrom;
+
+use get_input::get_raw_input;
+
+fn main() {
+    let input = get_raw_input(3, 1);
+
+    let tree_hits_3_1 = count_tree_hits(&input, 3, 1);
+    println!("{} trees are going to be hit", tree_hits_3_1);
+
+
+    let tree_hits_1_1 = count_tree_hits(&input, 1, 1);
+    let tree_hits_5_1 = count_tree_hits(&input, 5, 1);
+    let tree_hits_7_1 = count_tree_hits(&input, 7, 1);
+    let tree_hits_1_2 = count_tree_hits(&input, 1, 2);
+
+    println!("Tree hit score: {}", tree_hits_1_1 * tree_hits_1_2 * tree_hits_3_1 * tree_hits_5_1 * tree_hits_7_1)
+}
+
+#[derive(Eq, PartialEq, Debug)]
+enum MapItem {
+    Tree,
+    Snow,
+}
+
+impl TryFrom<char> for MapItem {
+    type Error = ();
+
+    fn try_from(value: char) -> Result<Self, Self::Error> {
+        match value {
+            '.' => Ok(MapItem::Snow),
+            '#' => Ok(MapItem::Tree),
+            _ => Err(())
+        }
+    }
+}
+
+fn count_tree_hits(base_map: &str, right: usize, down: usize) -> usize {
+    let map: Vec<Vec<MapItem>> = base_map.lines().into_iter()
+        .map(|line| line.chars().into_iter()
+            .map(|c| MapItem::try_from(c).expect(&format!("Could not parse {}", c)))
+            .collect())
+        .collect();
+    let mut tree_counter = 0;
+
+    let mut column_index = 0;
+    for row in map.iter().filter(|r| !r.is_empty()).step_by(down) {
+        match row.get(column_index) {
+            Some(tile) => { if *tile == MapItem::Tree { tree_counter += 1; } }
+            None => {
+                column_index %= row.len();
+                match row.get(column_index) {
+                    Some(tile) => { if *tile == MapItem::Tree { tree_counter += 1; } }
+                    None => panic!("something went horribly wrong")
+                }
+            }
+        }
+        column_index += right;
+    }
+
+    tree_counter
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = "
+..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#
+";
+
+    #[test]
+    fn should_count_test_input_default_slope() {
+        assert_eq!(count_tree_hits(TEST_INPUT, 3, 1), 7);
+    }
+
+    #[test]
+    fn should_count_test_input_slope_1_1() {
+        assert_eq!(count_tree_hits(TEST_INPUT, 1, 1), 2);
+    }
+
+    #[test]
+    fn should_count_test_input_slope_5_1() {
+        assert_eq!(count_tree_hits(TEST_INPUT, 5, 1), 3);
+    }
+
+    #[test]
+    fn should_count_test_input_slope_7_1() {
+        assert_eq!(count_tree_hits(TEST_INPUT, 7, 1), 4);
+    }
+
+    #[test]
+    fn should_count_test_input_slope_1_2() {
+        assert_eq!(count_tree_hits(TEST_INPUT, 1, 2), 2);
+    }
+
+    #[test]
+    fn should_work_with_empty_lines() {
+        assert_eq!(count_tree_hits(TEST_INPUT, 3, 1), 7);
+    }
+}
