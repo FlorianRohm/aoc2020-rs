a91778ae675bd4ce950800a06152327191cdb956
Florian Rohm <rohm.florian@googlemail.com>
2020-12-18 17:12:42 +0100

    day18 - 2

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,16 +1,21 @@
 #[macro_use]
 extern crate pest_derive;
 
-use parser::parse;
+
+use parser::AocParser;
 
 mod calculator;
 mod parser;
 
 fn main() {
     let input = include_str!("./input");
-    let all_evaluations: i64 = input.lines().map(|line| parse(line)).map(|argument| argument.eval()).sum();
+    let all_evaluations: i64 = input.lines().map(|line| parser::PREC_CLIMBER_PART_1.parse(line)).map(|argument| argument.eval()).sum();
 
     println!("All things evaluated are {}", all_evaluations);
+
+    let all_evaluations: i64 = input.lines().map(|line| parser::PREC_CLIMBER_PART_2.parse(line)).map(|argument| argument.eval()).sum();
+
+    println!("All things evaluated with the right arithmetic are {}", all_evaluations);
 }
 
 
@@ -19,11 +24,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn should_calculate() {
-        assert_eq!(parser::parse("1 + (2 * 3) + (4 * (5 + 6))").eval(), 51);
-        assert_eq!(parser::parse("2 * 3 + (4 * 5)").eval(), 26);
-        assert_eq!(parser::parse("5 + (8 * 3 + 9 + 3 * 4 * 3)").eval(), 437);
-        assert_eq!(parser::parse("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))").eval(), 12240);
-        assert_eq!(parser::parse("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2").eval(), 13632);
+    fn should_calculate_part1() {
+        assert_eq!(parser::PREC_CLIMBER_PART_1.parse("1 + (2 * 3) + (4 * (5 + 6))").eval(), 51);
+        assert_eq!(parser::PREC_CLIMBER_PART_1.parse("2 * 3 + (4 * 5)").eval(), 26);
+        assert_eq!(parser::PREC_CLIMBER_PART_1.parse("5 + (8 * 3 + 9 + 3 * 4 * 3)").eval(), 437);
+        assert_eq!(parser::PREC_CLIMBER_PART_1.parse("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))").eval(), 12240);
+        assert_eq!(parser::PREC_CLIMBER_PART_1.parse("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2").eval(), 13632);
+    }
+
+    #[test]
+    fn should_calculate_part2() {
+        assert_eq!(parser::PREC_CLIMBER_PART_2.parse("1 + (2 * 3) + (4 * (5 + 6)) ").eval(), 51);
+        assert_eq!(parser::PREC_CLIMBER_PART_2.parse("2 * 3 + (4 * 5) ").eval(), 46);
+        assert_eq!(parser::PREC_CLIMBER_PART_2.parse("5 + (8 * 3 + 9 + 3 * 4 * 3) ").eval(), 1445);
+        assert_eq!(parser::PREC_CLIMBER_PART_2.parse("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4)) ").eval(), 669060);
+        assert_eq!(parser::PREC_CLIMBER_PART_2.parse("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2 ").eval(), 23340);
     }
 }
--- day18/src/parser.rs
+++ day18/src/parser.rs
@@ -10,7 +10,7 @@ struct CalcParser;
 
 impl From<&str> for Operand {
     fn from(input: &str) -> Self {
-        match input {
+        match input.trim() {
             "+" => Operand::PLUS,
             "*" => Operand::TIMES,
             _ => panic!("operand not supported")
@@ -18,42 +18,63 @@ impl From<&str> for Operand {
     }
 }
 
-pub fn parse(input: &str) -> Argument {
-    let mut calculation = CalcParser::parse(Rule::Calculation, input).unwrap_or_else(|e| panic!("{}", e));
+lazy_static::lazy_static! {
+    pub static ref PREC_CLIMBER_PART_2: pest::prec_climber::PrecClimber<Rule> = {
+        use Rule::{Add, Multiply};
+        use pest::prec_climber::{Operator, Assoc};
+        use pest::prec_climber::PrecClimber;
 
-    parse_rules(calculation.into_iter().next().unwrap().into_inner())
+        PrecClimber::new(vec![
+            Operator::new(Multiply, Assoc::Left),
+            Operator::new(Add, Assoc::Left)
+        ])
+    };
+
+    pub static ref PREC_CLIMBER_PART_1: pest::prec_climber::PrecClimber<Rule> = {
+        use Rule::{Add, Multiply};
+        use pest::prec_climber::{Operator, Assoc};
+        use pest::prec_climber::PrecClimber;
+
+        PrecClimber::new(vec![
+            Operator::new(Multiply, Assoc::Left) | Operator::new(Add, Assoc::Left),
+
+        ])
+    };
 }
 
-fn parse_rules(mut input: pest::iterators::Pairs<Rule>) -> Argument {
-    let last = input.next_back();
-    let second_to_last = input.next_back();
-    match (last, second_to_last) {
-        (Some(last), Some(second)) => inner_to_argument(input, second, last),
-        (Some(l), None) => rule_to_argument(l),
+pub trait AocParser {
+    fn eval(&self, expression: Pairs<Rule>) -> Argument;
+    fn parse(&self, input: &str) -> Argument;
+}
 
-        _ => unimplemented!()
+impl AocParser for pest::prec_climber::PrecClimber<Rule> {
+    fn eval(&self, expression: Pairs<Rule>) -> Argument {
+        self.climb(
+            expression,
+            |pair: Pair<Rule>| match pair.as_rule() {
+                Rule::Digit => Argument::Literal(pair.as_str().trim().parse::<i64>().unwrap()),
+                Rule::SubExpr => self.eval(pair.into_inner()),
+                Rule::Expr => self.eval(pair.into_inner()),
+                _ => unreachable!(),
+            },
+            |lhs: Argument, op: Pair<Rule>, rhs: Argument| match op.as_rule() {
+                Rule::Add | Rule::Multiply => Argument::Expression {
+                    arg_1: Box::from(lhs),
+                    operand: op.as_str().into(),
+                    arg_2: Box::from(rhs),
+                },
+                _ => unreachable!(),
+            },
+        )
+    }
+
+    fn parse(&self, input: &str) -> Argument {
+        let mut calculation = CalcParser::parse(Rule::Calculation, input).unwrap_or_else(|e| panic!("{}", e));
+
+        self.eval(calculation.into_iter().next().unwrap().into_inner())
     }
 }
 
-fn rule_to_argument(input: Pair<Rule>) -> Argument {
-    match input.as_rule() {
-        Rule::Digit => Argument::Literal(input.as_str().trim().parse::<i64>().unwrap()),
-        Rule::SubExpr => parse_rules(input.into_inner()),
-        _ => unreachable!()
-    }
-}
-
-fn inner_to_argument(remainder: Pairs<Rule>, operand: Pair<Rule>, last: Pair<Rule>) -> Argument {
-    let arg_1 = parse_rules(remainder);
-    let arg_2 = rule_to_argument(last);
-    let operand = operand.as_str().trim().into();
-
-    Argument::Expression {
-        arg_1: Box::new(arg_1),
-        operand,
-        arg_2: Box::new(arg_2),
-    }
-}
 
 #[cfg(test)]
 mod tests {
@@ -63,8 +84,19 @@ mod tests {
     use super::*;
 
     #[test]
+    fn should_parse_simple_2() {
+        let parsed = PREC_CLIMBER_PART_2.parse("2 + 4");
+        assert_eq!(parsed,
+                   Argument::Expression {
+                       operand: Operand::PLUS,
+                       arg_1: Box::new(Argument::Literal(2)),
+                       arg_2: Box::new(Argument::Literal(4)),
+                   });
+    }
+
+    #[test]
     fn should_parse_simple() {
-        let parsed = parse("2 + 4");
+        let parsed = PREC_CLIMBER_PART_1.parse("2 + 4");
         assert_eq!(parsed,
                    Argument::Expression {
                        operand: Operand::PLUS,
@@ -75,7 +107,7 @@ mod tests {
 
     #[test]
     fn should_parse_sub_term() {
-        let parsed = parse("2 + (4*2)");
+        let parsed = PREC_CLIMBER_PART_1.parse("2 + (4*2)");
         assert_eq!(parsed,
                    Argument::Expression {
                        operand: Operand::PLUS,
@@ -90,7 +122,7 @@ mod tests {
 
     #[test]
     fn should_parse_multiple_term() {
-        let parsed = parse("2 * 3 + (4 * 5)");
+        let parsed = PREC_CLIMBER_PART_1.parse("2 * 3 + (4 * 5)");
         assert_eq!(parsed,
                    Argument::Expression {
                        operand: Operand::PLUS,
